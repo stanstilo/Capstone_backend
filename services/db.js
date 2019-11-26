246f9e05a5ca5ceda836f8c73c590197344c09f9
stanley <okstilo@gmail.com>
2019-11-26 03:47:54 -0800

    added endpoints

--- services/db.js
+++ services/db.js
@@ -11,50 +11,78 @@ pool.on('connect', () => {
 
 
   const Thing = () => {
-    const usersTable = `CREATE TABLE IF NOT EXISTS
-    users(
-      id serial PRIMARY KEY,
-      email VARCHAR(128) UNIQUE NOT NULL,
-      password VARCHAR(128) NOT NULL 
-    )`;
-  pool.query(usersTable)
-  .then((res) => {
-    console.log(res);
-    pool.end();
-  })
-  .catch((err) => {
-    console.log(err);
-    pool.end();
-  });
-}
-    //SIGNUP
-  //   const myTable = `CREATE TABLE IF NOT EXISTS
-  //       workers(
-  //          id serial PRIMARY KEY,
-  //          first_name VARCHAR(128) NOT NULL,
-  //          last_name VARCHAR(128) NOT NULL,
-  //          email VARCHAR(128) NOT NULL,
-  //          password VARCHAR(128) NOT NULL,
-  //          gender VARCHAR(128) NOT NULL,
-  //          job VARCHAR(128) NOT NULL,
-  //          dept VARCHAR(128) NOT NULL,
-  //          address VARCHAR(128) NOT NULL
-  //       )`;
+    const myTable = `CREATE TABLE IF NOT EXISTS
+        users(
+           id serial PRIMARY KEY,
+           email VARCHAR(128) NOT NULL,
+           password VARCHAR(128) NOT NULL,
+           UNIQUE(email)
+           
+        )`;
   
-  //     pool.query(myTable)
-  //      .then((res) => {
-  //        console.log(res);
-  //        pool.end();
-  //       })
-  //       .catch((err) => {
-  //       console.log(err);
-  //       pool.end();
-  //       });
-  //       }          
-  // pool.on('remove', () => {
-  //   console.log('client removed');
-  //   process.exit(0);
-  // });
+      pool.query(myTable)
+       .then((res) => {
+         console.log(res);
+         pool.end();
+        })
+        .catch((err) => {
+        console.log(err);
+        pool.end();
+        });
+
+    //SIGNUP
+    // const myTable = `CREATE TABLE IF NOT EXISTS
+    //     workers(
+    //        users_id serial PRIMARY KEY,
+    //        first_name VARCHAR(128) NOT NULL,
+    //        last_name VARCHAR(128) NOT NULL,
+    //        email VARCHAR(128) NOT NULL,
+    //        password VARCHAR(128) NOT NULL,
+    //        gender VARCHAR(128) NOT NULL,
+    //        job VARCHAR(128) NOT NULL,
+    //        dept VARCHAR(128) NOT NULL,
+    //        address VARCHAR(128) NOT NULL
+    //     )`;
+  
+    //   pool.query(myTable)
+    //    .then((res) => {
+    //      console.log(res);
+    //      pool.end();
+    //     })
+    //     .catch((err) => {
+    //     console.log(err);
+    //     pool.end();
+    //     });
+               
+  
+
+    //articles
+  
+          //  const usersTable = `CREATE TABLE IF NOT EXISTS
+          //  articles(
+          //   id serial PRIMARY KEY,
+          //   title text NOT NULL,
+          //   article text NOT NULL,
+          //   created_date timestamp NOT NULL, 
+          //   author VARCHAR(128) NOT NULL,
+          //   comment text NOT NULL
+          //  )`;
+        //  pool.query(usersTable)
+        //  .then((res) => {
+        //    console.log(res);
+        //    pool.end();
+        //  })
+        //  .catch((err) => {
+        //    console.log(err);
+        //    pool.end();
+        //  });
+        }
+        // pool.on('remove', () => {
+        //   console.log('client removed');
+        //   process.exit(0);
+        // });
+
+
   //export pool and Thing to be accessible  from any where within the application
   module.exports = {
     Thing,
