85343c41736aaf09e12f566b1cdfd614446ebc14
stanley <okstilo@gmail.com>
2019-11-30 00:45:54 -0800

    changed the endpoints

--- routes/articles.js
+++ routes/articles.js
@@ -2,16 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-const auth = require('../middleware/auth');
+// const auth = require('../middleware/auth');
 
 
 const articlesCtrl = require('../controllers/articles');
 
-router.get('/', auth, articlesCtrl.getArticles);
-router.post('/', auth, articlesCtrl.postArticles);
-router.post('/:articleId/comment', auth, articlesCtrl.postOneComment);
-router.delete('/:articleId', auth, articlesCtrl.deleteOneArticle);
-router.get('/:articlesId', auth, articlesCtrl.getOneArticle);
-router.patch('/:articleId', auth, articlesCtrl.updateOneArticle);
+router.get('/', articlesCtrl.getArticles);
+router.post('/', articlesCtrl.postArticles);
+router.post('/:articleId/comment', articlesCtrl.postOneComment);
+router.delete('/:articleId', articlesCtrl.deleteOneArticle);
+router.get('/:articlesId', articlesCtrl.getOneArticle);
+router.patch('/:articleId', articlesCtrl.updateOneArticle);
 
 module.exports = router;
